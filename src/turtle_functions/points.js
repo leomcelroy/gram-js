4543ede883e6785dacf985a10506453e88107156
leomcelroy <leomcelroy@gmail.com>
2021-10-17 23:10:57 -0400

    Update points.js

--- src/turtle_functions/points.js
+++ src/turtle_functions/points.js
@@ -1,37 +1,36 @@
-function flatten(items) {
-  const flat = [];
+// function flatten(items) {
+//   const flat = [];
 
-  items.forEach(item => {
-    if (Array.isArray(item)) {
-      flat.push(...flatten(item));
-    } else {
-      flat.push(item);
-    }
-  });
-
-  return flat;
-}
-
-// function flatten(arr) {
-//   var result = [];
-//   var stack = arr, first;
-
-//   while (stack.length > 0) {
-//     first = stack[0];
-
-//     if (Array.isArray(first)) {
-//       // Replace the nested array with its items
-//       Array.prototype.splice.apply(stack, [0, 1].concat(first));
+//   items.forEach(item => {
+//     if (Array.isArray(item)) {
+//       flat.push(...flatten(item));
 //     } else {
-//       result.push(first);
-//       // Delete the first item
-//       stack.splice(0, 1);
+//       flat.push(item);
 //     }
-//   }
+//   });
 
-//   return result;
+//   return flat;
 // }
 
+function flatten(arr) {
+  var result = [];
+  var stack = arr, first;
+
+  while (stack.length > 0) {
+    first = stack[0];
+
+    if (Array.isArray(first)) {
+      // Replace the nested array with its items
+      Array.prototype.splice.apply(stack, [0, 1].concat(first));
+    } else {
+      result.push(first);
+      // Delete the first item
+      stack.splice(0, 1);
+    }
+  }
+
+  return result;
+}
 
 export function points(turtle) {
 	return flatten(turtle.pathMap(x => x.points));
